8ae585d1bd4c65d089e86e2307dad4df10bb67fe
Israfilli Ramin <106513116+IsrafilliRamin@users.noreply.github.com>
2023-01-07 15:58:29 +0400

    Add files via upload

---
+++ JS-3cu ders/App.js
@@ -0,0 +1,230 @@
+/* const a = 5, b = 2, c = 'hello';
+
+// equal to operator
+console.log(a == 5);     // true
+console.log(b == '2');   // true
+console.log(c == 'Hello');  // false */
+
+
+
+/* const a1 = 3, b1 = 'hello';
+
+// not equal operator
+console.log(a1 != 2); // true
+console.log(b1 != 'Hello'); // true */
+
+
+/* const a2 = 2;
+
+// strict equal operator
+console.log(a2 === 2); // true
+console.log(a2 === '2'); //  false
+
+
+ const a3 = 2, b3 = 'hello';
+
+// strict not equal operator
+console.log(a3 != 2); //   false
+console.log(a3 !== '2'); // true
+console.log(b3 !== 'Hello'); // true */
+
+
+
+/* const a4 = 3;
+
+// greater than operator
+console.log(a4 > 2); //   true
+
+
+ const a5 = 3;
+
+// greater than or equal operator
+console.log(a5 >= 3); // true */
+
+
+
+/* const a6 = 3, b6 = 2;
+
+// less than operator
+console.log(a6 < 2); // false
+console.log(b6 < 3); // true
+ */
+
+
+
+/* const a7 = 2;
+
+// less than or equal operator
+console.log(a7 <= 3) // true
+console.log(a7 <= 2); // true */
+
+
+
+/* const a8 = true, b8 = false;
+const c8 = 4;
+
+// logical AND
+console.log(a8 && a8); // true
+console.log(a8 && b8);  // false
+
+console.log((c8 > 2) && (c8 < 2)); // false */
+
+
+
+/* const a9 = true, b9 = false, c9 = 4; */
+
+
+/* // logical OR
+console.log(a9 || b9); // true
+console.log(b9 || b9); // false
+console.log((c9>2) || (c9<2)); //  true */
+
+
+
+/* const a11 = true, b11 = false;
+
+// logical NOT
+console.log(!a11); // false
+console.log(!b11); // true  */
+
+
+
+
+
+
+
+/* js Hoisting */
+/* let a = 5;
+var b;
+b = 5;
+console.log(b); */
+
+/* Ternary */
+
+/* const age = 30;
+
+let resault = age > 35 ? "Yaslidi" : "Cavan";
+
+
+if(age > 35){
+   console.log();
+}else{
+
+}
+
+console.log(resault); */
+
+
+
+
+
+
+
+/* String Metods */
+
+let students = "Telebeler Mekteb Telebeler Telebeler";
+
+/* length */
+
+/* console.log(students.length); */
+
+/* slice */
+/* parametler start ve end */
+/* console.log(students.slice(0,20)); */
+
+/*  substring */
+
+/* console.log(students.substring(0,10)); */
+
+/* substr */
+/* start ve length */
+/* console.log(students.substr(0,9)); */
+
+/*   replace */
+
+console.log(students.replace("Telebeler", "3-cu kurs" ));
+
+/* replaceAll  */
+
+/*  toUpperCase */  /* console.log(students.toUpperCase()); */
+/*  toLoweCase */    /* console.log(students.toLowerCase()); */
+
+
+/*  Concat */
+
+
+let name1 = "Ramin";
+let surname1 = "Israfilli";
+
+let allName = name1.concat(" ",surname1);
+
+console.log(allName);
+
+
+/*  Trim */
+
+/* let x = "salam     ";
+console.log(x.length);
+let y = x.trimStart();
+console.log(y.length); */
+
+/* trimEnd */
+/* trimStart */
+
+
+
+
+/*    charAt */
+
+/* let ad = "Elsen";
+
+console.log(ad.charAt()); */
+
+
+/* split */
+
+/* let ad = "Elsen";
+
+let newAD = ad.split(" ");
+
+console.log(newAD); */
+
+
+/* includes */
+
+/* let telebler = "Elsen Aynur Elnur";
+
+let x = telebler.includes("Elsen");
+
+console.log(x); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
